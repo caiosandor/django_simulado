45f6b6c6895e56469a625356df2d182364d9d2a3
caio <caiosandor16@gmail.com>
2026-09-16 20:45:59 -0300

    forms e base html

--- biblioteca/acervo/urls.py
+++ biblioteca/acervo/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path('', views.inicio),
+    path(
+        'livros/',
+        views.lista_livros,
+        name='lista'
+    ),
 ]
--- biblioteca/acervo/views.py
+++ biblioteca/acervo/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
-def inicio(request):
-    return HttpResponse(
-'Olá, acervo!'
-)
+from django.shortcuts import render, redirect
+from .models import Livro
+from .forms import LivroForm
+
+# View para listar os livros
+def lista_livros(request):
+    livros = Livro.objects.all() # busca no banco
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros} # envia ao template
+    )
+
+# View para processar o formulário (GET mostra, POST salva)
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save() # grava no banco
+            return redirect('lista')
+    else:
+        form = LivroForm()
+    return render(request, 'acervo/form.html', {'form': form})
