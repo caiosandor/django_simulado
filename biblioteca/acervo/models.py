76c40bd0198394df0cd3e2594006a68ef84cddd1
caio <caiosandor16@gmail.com>
2026-09-16 21:27:32 -0300

    corrigindo o model de novo

--- biblioteca/acervo/models.py
+++ biblioteca/acervo/models.py
@@ -1,31 +1,22 @@
 from django.db import models
+
 class Livro(models.Model):
-    titulo = models.CharField(max_length=200)
-    autor = models.CharField(max_length=100)
-    ano = models.IntegerField()
-    disponivel = models.BooleanField(default=True)
-    def __str__(self):
-        return self.titulo
-
-
-
-    class Livro(models.Model):
-        TIPO_CHOICES = [
-            ('Físico', 'Físico'),
-            ('Digital', 'Digital'),
+    TIPO_CHOICES = [
+        ('Físico', 'Físico'),
+        ('Digital', 'Digital'),
     ]
 
     CATEGORIA_CHOICES = [
-        ('000', '000 – Generalidades e Informação'),
-        ('100', '100 – Filosofia e Psicologia'),
-        ('200', '200 – Religião e Teologia'),
-        ('300', '300 – Ciências Sociais e Direito'),
-        ('400', '400 – Linguística e Idiomas'),
-        ('500', '500 – Ciências Puras (Exatas e Naturais)'),
-        ('600', '600 – Ciências Aplicadas (Tecnologia)'),
-        ('700', '700 – Artes e Recreação'),
-        ('800', '800 – Literatura'),
-        ('900', '900 – História e Geografia'),
+        ('0', '0 – Generalidades e Informação'),
+        ('1', '1 – Filosofia e Psicologia'),
+        ('2', '2 – Religião e Teologia'),
+        ('3', '3 – Ciências Sociais e Direito'),
+        ('4', '4 – Linguística e Idiomas'),
+        ('5', '5 – Ciências Puras (Exatas e Naturais)'),
+        ('6', '6 – Ciências Aplicadas (Tecnologia)'),
+        ('7', '7 – Artes e Recreação'),
+        ('8', '8 – Literatura'),
+        ('9 ', '9– História e Geografia'),
     ]
 
     titulo = models.CharField(max_length=200, verbose_name="Título")
