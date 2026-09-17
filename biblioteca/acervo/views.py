6f4d968fd1f02c6304172270a833ec5869f7d99f
caio <caiosandor16@gmail.com>
2026-09-16 21:31:05 -0300

    adicionando view e consertando erros

--- biblioteca/acervo/views.py
+++ biblioteca/acervo/views.py
@@ -2,21 +2,35 @@ from django.shortcuts import render, redirect
 from .models import Livro
 from .forms import LivroForm
 
-# View para listar os livros
 def lista_livros(request):
-    livros = Livro.objects.all() # busca no banco
-    return render(
-        request, 'acervo/lista.html',
-        {'livros': livros} # envia ao template
-    )
+    livros = Livro.objects.all()
 
-# View para processar o formulário (GET mostra, POST salva)
+    # Capturando os valores enviados pelo formulário de busca
+    nome = request.GET.get('nome', '')
+    tipo = request.GET.get('tipo', '')
+    categoria = request.GET.get('categoria', '')
+
+    # Aplicando os filtros
+    if nome:
+        livros = livros.filter(titulo__icontains=nome)
+    if tipo:
+        livros = livros.filter(tipo=tipo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+
+    contexto = {
+        'livros': livros,
+        'request': request,
+    }
+    return render(request, 'acervo/lista.html', contexto)
+
 def novo_livro(request):
     if request.method == 'POST':
         form = LivroForm(request.POST)
         if form.is_valid():
-            form.save() # grava no banco
+            form.save()
             return redirect('lista')
     else:
         form = LivroForm()
+    
     return render(request, 'acervo/form.html', {'form': form})
